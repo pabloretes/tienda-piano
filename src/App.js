f995dcba1d83e9df9aee80dc1ebbf71d3ac72674
Pablo Retes <Pablo.Retes@gmail.com>
2022-07-15 20:22:46 -0300

    Menú e-commerce y Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/NavBar.css';
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Nav from 'react-bootstrap/Nav';
 import Logo from '../images/logo.png';
 import "../styles/footer.css";
 
@@ -12,20 +11,26 @@ function Footer() {
        
           <Row className="row-footer">
             <Col sm={4}>
-                <div >
+                <div>
                     <img src={Logo} className="logo-imagen" alt="logo" />
                 </div>
                 <p>Siguenos en:</p>
             </Col>
             <Col sm>
-                <Nav defaultActiveKey="/home" className="flex-column">
-                    <Nav.Link href="/home">Pianos</Nav.Link>
-                    <Nav.Link eventKey="link-1">Pianos de Cola</Nav.Link>
-                    <Nav.Link eventKey="link-2">Pianos Vertivales</Nav.Link>
-                </Nav>
+                
+                   <p><a className="texto-blanco">Pianos</a></p>
+                   <p><a className="texto-blanco">Pianos de Cola</a></p>
+                   <p><a className="texto-blanco">Pianos Verticales</a></p>
+               
             </Col>
-            <Col sm>Footer en Construccion Linea 1</Col>
-            <Col sm>Footer en Construccion Linea 1</Col>
+            <Col sm>
+                <p><a className="texto-blanco">Pianos</a></p>
+                <p><a className="texto-blanco">Pianos de Cola</a></p>
+            </Col>
+            <Col sm>
+            <p><a className="texto-blanco">Pianos</a></p>
+                   <p><a className="texto-blanco">Pianos de Cola</a></p>
+            </Col>
           </Row>
           <Row>
             <Col>© 2022 Tienda Piano - All Rights Reserved</Col>
