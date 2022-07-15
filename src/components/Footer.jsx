4c630720fdbbaad7e4ee0dd9ca08ab78bc75aff3
Pablo Retes <Pablo.Retes@gmail.com>
2022-07-15 19:47:56 -0300

    Menú e-commerce y Crea tu landing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import '../styles/footer.css';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from 'react-bootstrap/Nav';
+import Logo from '../images/logo.png';
+import "../styles/footer.css";
 
 function Footer() {
   return (
-    
-    <div className='footer-container'>Footer en Construcción</div>
-  )
+    <>
+      <div className="footer-container">
+       
+          <Row className="row-footer">
+            <Col sm={4}>
+                <div >
+                    <img src={Logo} className="logo-imagen" alt="logo" />
+                </div>
+                <p>Siguenos en:</p>
+            </Col>
+            <Col sm>
+                <Nav defaultActiveKey="/home" className="flex-column">
+                    <Nav.Link href="/home">Pianos</Nav.Link>
+                    <Nav.Link eventKey="link-1">Pianos de Cola</Nav.Link>
+                    <Nav.Link eventKey="link-2">Pianos Vertivales</Nav.Link>
+                </Nav>
+            </Col>
+            <Col sm>Footer en Construccion Linea 1</Col>
+            <Col sm>Footer en Construccion Linea 1</Col>
+          </Row>
+          <Row>
+            <Col>© 2022 Tienda Piano - All Rights Reserved</Col>
+          </Row>
+       
+      </div>
+    </>
+  );
 }
 
 export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../images/logo.svg";
+import logo from "../images/logo-nav.png";
 import Nav from 'react-bootstrap/Nav';
+import { NavDropdown } from "react-bootstrap";
 import CartWidget from "../components/CartWidget";
 import React from 'react';
 
@@ -11,18 +12,25 @@ function NavBar() {
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            Tienda Piano
-          </Navbar.Brand>
+              <img
+                alt=""
+                src={logo}
+                width="80"
+                className="d-inline-block align-center"
+              />{" "}
+            </Navbar.Brand>
+            <div className="logo-texto">
+              TIENDAPIANO
+            </div>
           <Nav className="me-auto">
-            <Nav.Link href="#">Categorias</Nav.Link>
-            <Nav.Link href="#">Contacto</Nav.Link>
+           <NavDropdown title="CATEGORIAS" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Pianos de Cola</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Pianos Verticales</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Pianos Económicos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Minipianos</NavDropdown.Item>
+            </NavDropdown>
+             
+            <Nav.Link href="#">CONTACTO</Nav.Link>
           </Nav>
             <CartWidget  items={3}/>
         </Container>
